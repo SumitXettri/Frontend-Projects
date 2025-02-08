98819518a591d9bb409d9d9ca9db1b6c96b9cd98
Sumit Chhetri <sumitchhetri277@gmail.com>
2025-02-08 21:05:09 +0545

    Update App.jsx

--- Piano-App/src/App.jsx
+++ Piano-App/src/App.jsx
@@ -1,1 +1,18 @@
+import { useState } from "react";
+import "./styles/App.css";
+import Piano from "./components/Piano";
 
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="text-center text-3xl font-bold underline heading">
+        Piano App
+      </h1>
+      <Piano />
+    </div>
+  );
+}
+
+export default App;
